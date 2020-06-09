966f55c250e4814aac9d3a613d3cbcc715387680
MG <Magn3s1um-g@users.noreply.github.com>
2020-06-09 11:53:25 +0100

    initial commit of website files
    This is my first commit of my website files.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Function to switch the contents of the hero text, span section, every 3 seconds
+var i = 0;
+function heroSwitcher() {
+  var heroText = document.getElementById("span-hero");
+  var heroArray = [
+    "a web developer ⌨️",
+    "a big, big fan of coffee ☕",
+    "in love with pixels 💻",
+    "for hire! 🧑🏻‍",
+    "ready to go 🚀",
+  ];
+  heroText.innerText = heroArray[i];
+  i = (i + 1) % heroArray.length;
+}
+setInterval(heroSwitcher, 3000);
