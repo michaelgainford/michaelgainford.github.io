1fb823c928d5d6cef40ec85b1a2cc11d15b708e4
MG <64011978+Magn351um@users.noreply.github.com>
2021-02-18 22:07:01 +0000

    Made small amendment to text in header

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Function to switch the contents of the hero text, span section, every 3 seconds
-var i = 0;
+var i = 0
 function heroSwitcher() {
-  var heroText = document.getElementById("header__hero-text__span");
+  var heroText = document.getElementById("header__hero-text__span")
   var heroArray = [
     "a web developer ⌨️",
     "a big fan of coffee  ☕",
     "a lover of pixels 💻",
     "for hire 😃",
-    "ready to go 🚀",
-  ];
-  heroText.innerText = heroArray[i];
-  i = (i + 1) % heroArray.length;
+    "ready to launch 🚀",
+  ]
+  heroText.innerText = heroArray[i]
+  i = (i + 1) % heroArray.length
 }
-setInterval(heroSwitcher, 3000);
+setInterval(heroSwitcher, 3000)
 
 // Next function
