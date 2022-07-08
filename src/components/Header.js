0e7f3ceff059b1a5c7c8e2a0e9b77d8129dc18eb
Michael Gainford <code@michaelgainford.co.uk>
2022-07-08 08:56:22 +0100

    Created heaver nav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,12 @@
-import { Link } from "react-router-dom";
 import MainLogo from "./Logo";
-import { FaHome } from "react-icons/fa";
-import { FaTools } from "react-icons/fa";
-import { FaCode } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import HeaderNav from "./HeaderNav";
 
 const Header = () => {
   return (
     <div className="logo-nav bg-theme md:h-screen w-full md:w-1/5 md:sticky md:top-0 grid auto-rows-auto">
       <MainLogo />
-      <nav className="nav-main grid grid-cols-4 md:grid-cols-1 max-h-full tracking-widest text-xs md:text-sm text-highlight grid-flow-row auto-rows-max">
-        <Link
-          to="/"
-          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
-        >
-          <span className="text-highlight">
-            <FaHome />
-          </span>
-          Home
-        </Link>
-        <Link
-          to="/code"
-          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
-        >
-          <span className="text-highlight">
-            <FaCode />
-          </span>
-          Code
-        </Link>
-        <Link
-          to="/resources"
-          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
-        >
-          <span className="text-highlight">
-            <FaTools />
-          </span>
-          Resources
-        </Link>
-        <Link
-          to="/"
-          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
-        >
-          <span className="text-highlight">
-            <FaTwitter />
-          </span>
-          Twitter
-        </Link>
-      </nav>
+      <HeaderNav />
+
     </div>
   );
 };
---
+++ src/components/HeaderNav.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { FaTools } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+const HeaderNav = () => {
+    return (
+        <nav className="nav-main grid grid-cols-4 md:grid-cols-1 max-h-full tracking-widest text-xs md:text-sm text-highlight grid-flow-row auto-rows-max">
+        <Link
+          to="/"
+          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
+        >
+          <span className="text-highlight">
+            <FaHome />
+          </span>
+          Home
+        </Link>
+        <Link
+          to="/code"
+          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
+        >
+          <span className="text-highlight">
+            <FaCode />
+          </span>
+          Code
+        </Link>
+        <Link
+          to="/resources"
+          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
+        >
+          <span className="text-highlight">
+            <FaTools />
+          </span>
+          Resources
+        </Link>
+        <Link
+          to="/"
+          className="lowercase md:border-t border-theme-alt hover:bg-theme-alt text-light text-center px-4 py-12 flex flex-col justify-center items-center ease-in-out duration-500"
+        >
+          <span className="text-highlight">
+            <FaTwitter />
+          </span>
+          Twitter
+        </Link>
+      </nav>
+    );
+};
+
+export default HeaderNav;
