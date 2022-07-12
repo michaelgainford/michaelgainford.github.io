c6be90ea7d3d4e245dc6394a33beb139cb957255
Michael Gainford <code@michaelgainford.co.uk>
2022-07-12 20:05:23 +0100

    Push finished (for now) projects sections

--- src/components/MyProjectsMain.js
+++ src/components/MyProjectsMain.js
@@ -38,16 +38,16 @@ const MyProjectsMain = () => {
           return (
             <div
               key="myProjectMain.id"
-              className="project flex flex-col items-center hover:gap-y-4 hover:bg-theme-alt border-2 border-gray-500 hover:border-theme-alt rounded-xl p-4 md:hover:pb-0 ease-in-out duration-500 lowercase hover:uppercase"
+              className="project flex flex-col items-center mb-4 md:mb-8 hover:bg-theme-alt border-2 border-transparent hover:border-theme-alt rounded-xl ease-in-out duration-500 lowercase hover:uppercase"
             >
               <a
                 href={myProjectMain.url}
-                className="p-2 sm:p-8 md:hover:p-8 md:hover:pb-0 text-light hover:text-highlight ease-in-out duration-500"
+                className="p-2 sm:p-8 md:p-8 text-light hover:text-highlight ease-in-out duration-500"
               >
                 <img
                   src={myProjectMain.image}
                   alt="template"
-                  className="w-full h-auto border-lg rounded-xl object-cover border-[8px] border-theme-alt hover:border-highlight opacity-60 hover:opacity-100"
+                  className="w-full h-auto border-lg rounded-xl object-cover border-[8px] md:border-[12px] border-theme-alt hover:border-highlight opacity-60 hover:opacity-100 ease-in-out duration-500"
                 />
                 <p className="text-center lowercase tracking-widest text-sm p-2 sm:p-4 md:p-8 w-full">
                   {myProjectMain.projectName}
--- src/components/MyProjectsSub.js
+++ src/components/MyProjectsSub.js
@@ -44,20 +44,20 @@ const MyProjectsSub = () => {
       <h3 className="text-center text-theme-alt text-4xl font-bold lowercase mt-4 pb-4 mb-4 border-b-4 border-theme-alt w-fit mx-auto">
         Sub Projects
       </h3>
-      <div className="projects-wrapper grid md:grid-cols-3 p-4">
+      <div className="projects-wrapper grid md:grid-cols-3 md:gap-4">
         {Object.values(myProjectsSubInfo).map((myProjectSub) => {
           return (
             <div
               key="myProjectSub.id"
-              className="project p-4 md:p-16 md:hover:p-8 flex flex-col justify-center items-center mb-0 hover:-mb-4 md:hover:pb-0 ease-in-out duration-500 rounded-xl border-2 border-transparent hover:border-theme-alt hover:bg-theme-alt lowercase hover:uppercase hover:gap-y-4"
+              className="project flex flex-col items-center mb-4 md:mb-8 hover:bg-theme-alt border-2 border-transparent hover:border-theme-alt rounded-xl ease-in-out duration-500 lowercase hover:uppercase"
             >
-              <a href={myProjectSub.url}>
+              <a href={myProjectSub.url} className="p-2 sm:p-8 md:p-8 text-light hover:text-highlight ease-in-out duration-500">
                 <img
                   src={myProjectSub.image}
                   alt="template"
-                  className="w-full h-auto border-lg rounded-lg border-[8px] border-theme-alt hover:border-highlight opacity-60 hover:opacity-100 ease-in-out duration-500"
+                  className="w-full h-auto border-lg rounded-xl object-cover border-[4px] md:border-[6px] border-theme-alt hover:border-highlight opacity-60 hover:opacity-100 ease-in-out duration-500"
                 />
-                <p className="text-center lowercase text-light tracking-widest text-sm p-4 w-full ease-in-out duration-500">
+                <p className="text-center lowercase tracking-widest text-sm p-2 sm:p-4 md:p-8 w-full">
                   {myProjectSub.projectName}
                 </p>
               </a>
