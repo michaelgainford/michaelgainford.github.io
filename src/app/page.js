b347409dde7bf6feaeef920c07fe1c2130b6103d
MG <code@michaelgainford.co.uk>
2023-03-04 11:35:24 +0000

    Start

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,124 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+//import { Inter } from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className="grid grid-cols-12">
+      <div className="sidebar h-screen col-span-1">
+        <ul className="grid dvh-full">
+          <li className="flex justify-center items-center">
             <Image
-              src="/vercel.svg"
+              src="/m-grey-placeholder.png"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
               priority
             />
-          </a>
+          </li>
+          <li className="flex justify-center items-center">
+            <Image
+              src="/m-grey-placeholder.png"
+              alt="Vercel Logo"
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
+              priority
+            />
+          </li>
+          <li className="flex justify-center items-center">
+            <Image
+              src="/m-grey-placeholder.png"
+              alt="Vercel Logo"
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
+              priority
+            />
+          </li>
+          <li className="flex justify-center items-center">
+            <Image
+              src="/m-grey-placeholder.png"
+              alt="Vercel Logo"
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
+              priority
+            />
+          </li>
+          <li className="flex justify-center items-center">
+            <Image
+              src="/m-grey-placeholder.png"
+              alt="Vercel Logo"
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
+              priority
+            />
+          </li>
+          <li className="flex justify-center items-center">
+            <Image
+              src="/m-grey-placeholder.png"
+              alt="Vercel Logo"
+              className="logo h-12 w-12 fill-red-500"
+              width={48}
+              height={48}
+              priority
+            />
+          </li>
+        </ul>
+      </div>
+      <div className="content min-h-screen border border-green-500 col-span-11">
+        <div className="header-bar w-full p-8 border border-red-500">
+          <h1 class="text-2xl text-slate-400 font-medium text-center">
+            Useful Code
+          </h1>
+        </div>
+        <div className="actual-content p-8">
+          <div>
+            <ul className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+              <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
+                List Item
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
