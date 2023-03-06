713e017ac9ca548ba36cd80880952f4a2bbe50ac
MG <code@michaelgainford.co.uk>
2023-03-06 09:37:17 +0000

    Push

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
         </div>
         <div className="actual-content p-8">
           <div>
-            <ul className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <ul className="resource-links grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               <li className="bg-slate-800 rounded-lg p-4 min-h-[120px] text-white flex items-center justify-center">
                 List Item
               </li>
