b198478d088b6510a87a27b9ff48db4cbafac6b4
MG <code@michaelgainford.co.uk>
2023-12-28 23:41:36 +0000

    Lots of progress so adding all text content

---
+++ js/euro-2024.js
@@ -0,0 +1,107 @@
+const euro2024Teams = [
+  {
+    id: 1,
+    name: "Albania",
+    group: "B",
+  },
+  {
+    id: 2,
+    name: "Austria",
+    group: ""
+  },
+  {
+    id: 3,
+    name: "Belgium",
+    group: ""
+  },
+  {
+    id: 4,
+    name: "Croatia",
+    group: ""
+  },
+  {
+    id: 5,
+    name: "Czech Republic",
+    group: ""
+  },
+  {
+    id: 6,
+    name: "Denmark",
+    group: ""
+  },
+  {
+    id: 7,
+    name: "England",
+    group: ""
+  },
+  {
+    id: 8,
+    name: "France",
+    group: ""
+  },
+  {
+    id: 9,
+    name: "Germany",
+    group: ""
+  },
+  {
+    id: 10,
+    name: "Hungary",
+    group: ""
+  },
+  {
+    'id': 11,
+    'name': "Italy",
+    'group': ""
+  },
+  {
+    id: 12,
+    name: "Netherlands",
+    group: ""
+  },
+  {
+    id: 13,
+    name: "Portugal",
+    group: ""
+  },
+  {
+    id: 14,
+    name: "Romania",
+    group: ""
+  },
+  {
+    id: 15,
+    name: "Scotland",
+    group: ""
+  },
+  {
+    id: 16,
+    name: "Serbia",
+    group: ""
+  },
+  {
+    id: 17,
+    name: "Slovakia",
+    group: ""
+  },
+  {
+    id: 18,
+    name: "Slovenia",
+    group: ""
+  },
+  {
+    id: 19,
+    name: "Spain",
+    group: "",
+  },
+  {
+    id: 20,
+    name: "Switzerland",
+    group: ""
+  },
+  {
+    id: 21,
+    name: "Turkey",
+    group: ""
+  }
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./.{html,js}"],
+  content: [
+    './**/*.{html,js}',
+    './**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
