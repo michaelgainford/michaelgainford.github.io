513871b4f1506d383e07f8df57e10fec4b720b31
MG <code@michaelgainford.co.uk>
2024-04-26 19:26:03 +0100

    Commit component based changes

--- components/typography/H1Span.js
+++ components/typography/H1Span.js
@@ -1,5 +1,5 @@
 export default function H1Span({ text }) {
   return (
-    <h1 className="pt-8 font-light leading-relaxed tracking-wide text-left text-md max-lg:font-bold sm:text-md md:text-xl lg:font-medium lg:text-3xl max-lg:mb-1 md:pt-14 lg:py-16">{text}</h1>
+    <h1 className="pt-8 font-light leading-relaxed tracking-wide text-left text-md max-lg:font-bold sm:text-md md:text-xl lg:font-medium lg:text-3xl max-lg:mb-4 md:pt-14 lg:py-16">{text}</h1>
   )
 }
