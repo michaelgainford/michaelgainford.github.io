c5db55c2410d6d5ac3cdd7ae6d1a06ad19a73812
MG <code@michaelgainford.co.uk>
2024-04-28 22:57:50 +0100

    Quick tweaks to fix footer on mobile

--- components/footer/FooterNavListHeader.js
+++ components/footer/FooterNavListHeader.js
@@ -1,5 +1,5 @@
 export default function FooterNavListHeader({ text }) {
   return (
-    <h4 className="pb-4 mb-0 text-xs tracking-widest text-left uppercase border-b lg:text-left border-slate-700 font-extralight">{text}</h4>
+    <h4 className="max-sm:w-full pb-4 mb-0 text-xs tracking-widest text-left uppercase border-b lg:text-left border-slate-700 font-extralight">{text}</h4>
   )
 }
--- components/footer/MainFooterNav.js
+++ components/footer/MainFooterNav.js
@@ -52,8 +52,23 @@ export default function MainFooterNav() {
   return (
     <div className="col-span-1 footer-information lg:col-span-2 lg:pr-8">
       <FooterSectionHeader text="Information" />
-      <div className="footer-navs grid grid-cols-2 gap-16 lg:flex-row footer-nav lg:grid lg:grid-cols-2 max-sm:mb-4">
-        <div className="rounded lg:pr-8 footer-nav-group border-slate-700">
+      <div className="footer-navs grid grid-cols-1 sm:grid-cols-2 gap-16 lg:flex-row footer-nav lg:grid lg:grid-cols-2 max-sm:mb-4">
+        <details className="sm:hidden w-full">
+          <summary className="flex w-full">
+            <FooterNavListHeader text="Fun Stuff" />
+            </summary>
+          <ul className="text-xs text-left md:text-sm lg:text-left">
+            {footerNavItemsOne.map((item, index) => (
+              <li key={index}>
+                <Link className={`${linkClasses}`} href
+                ={item.href}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </details>
+        <div className="rounded lg:pr-8 footer-nav-group border-slate-700 max-sm:hidden">
           <FooterNavListHeader text="Fun Stuff" />
           <ul className="text-xs text-left md:text-sm lg:text-left">
             {footerNavItemsOne.map((item, index) => (
@@ -66,7 +81,22 @@ export default function MainFooterNav() {
             ))}
           </ul>
         </div>
-        <div className="rounded lg:pr-8 footer-nav-group border-slate-700">
+        <details className="sm:hidden w-full">
+          <summary className="flex w-full">
+            <FooterNavListHeader text="Boring Stuff" />
+          </summary>
+          <ul className="text-xs text-left md:text-sm lg:text-left">
+            {footerNavItemsTwo.map((item, index) => (
+              <li key={index}>
+                <Link className={`${linkClasses}`} href={item.href
+                }>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </details>
+        <div className="rounded lg:pr-8 footer-nav-group border-slate-700 max-sm:hidden">
           <FooterNavListHeader text="Boring Stuff" />
           <ul className="text-xs text-left md:text-sm lg:text-left">
             {footerNavItemsTwo.map((item, index) => (
