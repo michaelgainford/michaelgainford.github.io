a3093aca1d493e1a00614c17cce26513ac72116e
MG <code@michaelgainford.co.uk>
2024-08-16 18:40:33 +0100

    Clean out Main

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-  var x = document.getElementById("top-nav")
-  if (x.className === "top-nav") {
-    x.className += " responsive"
-  } else {
-    x.className = "top-nav"
-  }
-}
