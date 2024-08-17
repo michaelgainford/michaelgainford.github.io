b5c3a238d9fe8106445498ed37ed53b181e87c28
Michael Gainford <m1cha3lg@outlook.com>
2024-08-17 10:18:32 +0100

    Push all data files

---
+++ components/data/DataForMyDevDashboard.js
@@ -0,0 +1,60 @@
+const DataForMyDevDashboard = [
+  {
+    name: "Fairway Golf Group Holidays",
+    url: "fairwaygolfgroupholidays.co.uk",
+    image: "/dev-resources/screenshot-of-fairway-golf-group-holidays-2x.webp",
+    expires: "03/06/2024",
+    domain_registrar: "Krystal",
+    host: "Krystal",
+  },
+  {
+    name: "Golf Membership North West",
+    url: "golfmembershipnorthwest.co.uk",
+    image: "/dev-resources/screenshot-of-golf-membership-2x.webp",
+    expires: "04/02/2025",
+    domain_registrar: "Krystal",
+    host: "Krystal",
+  },
+  {
+    name: "MichaelGainford.dev",
+    url: "michaelgainford.dev",
+    image: "/dev-resources/screenshot-of-dev-site-2x.webp",
+    expires: "1/1/2022",
+    domain_registrar: "Squarespace",
+    host: "GitHub",
+  },
+  {
+    name: "Portfolio Site",
+    url: "michaelgainford.co.uk",
+    image: "/dev-resources/screenshot-of-portfolio-site-2x.webp",
+    expires: "24/02/2025",
+    domain_registrar: "Krystal",
+    host: "Krystal",
+  },
+  {
+    name: "Show Me Sport",
+    url: "showmesport.com",
+    image: "/placeholder-files/square-template.webp",
+    expires: "1/1/2022",
+    domain_registrar: "Google Domains",
+    host: "N/A",
+  },
+  {
+    name: "Pigeons Playground",
+    url: "pigeonsplayground.com",
+    image: "/dev-resources/screenshot-of-pigeons-playground-2x.webp",
+    expires: "22/05/2024",
+    domain_registrar: "Krystal",
+    host: "Krystal",
+  },
+  {
+    name: "Sean Coyle Golf Events",
+    url: "seancoylegolfevents.com",
+    image: "/placeholder-files/square-template.webp",
+    expires: "11/12/2024",
+    domain_registrar: "Krystal",
+    host: "N/A",
+  },
+]
+
+export default DataForMyDevDashboard;
---
+++ components/data/DataForSocials.js
@@ -0,0 +1,30 @@
+
+import TwitterIcon from '/components/icons/IconTwitter'
+import CodepenIcon from '/components/icons/IconCodepen'
+import GitHubIcon from '/components/icons/IconGitHub'
+import LinkedInIcon from '/components/icons/IconLinkedIn'
+
+const DataForSocials = [
+  {
+    name: "Codepen",
+    href: "https://codepen.io/michaelgainford",
+    icon: CodepenIcon
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/michaelgainford",
+    icon: GitHubIcon
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/michaelgainford/",
+    icon: LinkedInIcon
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/m1cha3lgainford",
+    icon: TwitterIcon
+  }
+]
+
+export default DataForSocials;
