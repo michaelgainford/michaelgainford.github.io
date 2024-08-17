b1efef4f3c00d3c66348a9d02c820e3f2f97556f
Michael Gainford <m1cha3lg@outlook.com>
2024-08-17 10:17:45 +0100

    Push Tailwind config and global css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "brand-background": "url('/svg-backgrounds/constellation.svg')",
+        "brick-wall-background": "url('/svg-backgrounds/brick-wall.svg')",
+        "dots-background": "url('/svg-backgrounds/dots.svg')",
+        "graph-paper-background": "url('/svg-backgrounds/graph-paper.svg')",
+        "hexagons-background": "url('/svg-backgrounds/hexagons.svg')",
+        "movies-background": "url('/svg-backgrounds/kiwi.svg')",
+        "melt-background": "url('/svg-backgrounds/melt.svg')",
+        "rain-background": "url('/svg-backgrounds/rain.svg')",
+        "topo-background": "url('/svg-backgrounds/topography.svg')",
+      },
+      container: {
+        center: true,
+      },
+      screens: {
+        '3xl': '1500px',
       },
     },
   },
