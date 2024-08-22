6d9834a06c5576657c51d9a096e5b88a828bfbb6
Michael Gainford <m1cha3lg@outlook.com>
2024-08-22 23:31:12 +0100

    Output Prem data

--- app/projects/football/competitions/premier-league/page.js
+++ app/projects/football/competitions/premier-league/page.js
@@ -4,65 +4,112 @@ import H1 from "@/components/typography/H1Span";
 import MainFooter from "@/components/footer/MainFooter";
 import { mainStyles } from "@/components/data/Variables";
 import { globalPageStyles } from "@/components/data/Variables";
+import EnglishPremierLeagueBySeasons from "@/components/data/DataForPremTeamsBySeason";
 
-import Euro2024Teams from "@/components/data/DataForEuro2024";
-
-// Sort by group and then group seeding
-Euro2024Teams.sort((a, b) => {
-  if (a.group > b.group) {
+// sort by teamName
+EnglishPremierLeagueBySeasons.sort((a, b) => {
+  if (a.teamName > b.teamName) {
     return 1;
-  } else if (a.group < b.group) {
+  } else if (a.teamName < b.teamName) {
     return -1;
   } else {
-    return a.groupSeeding - b.groupSeeding;
+    return 0;
   }
-}
-);
+});
 
-const groups = [...new Set(Euro2024Teams.map((team) => team.group))];
-groups.sort();
+const teams = [...new Set(EnglishPremierLeagueBySeasons.map((team) => team.teamName))];
+teams.sort();
 
 export default function Home() {
   return (
     <div className={`bg-dots-background bg-blue-800 ${globalPageStyles}`}>
-
       <MainHeader />
-
       <main className={`${mainStyles}`}>
-
-        <H1 text="Euro 2024" />
-
-        <div className="hidden grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-12 first-line:gap-4 countries-grid-container">
-          {Euro2024Teams.map((team, index) => (
-            <div key={index} className="flex flex-col items-center w-full pb-4 duration-300 ease-in-out border border-blue-900 rounded-md cursor-pointer movie-card text-light_colour border-secondary group hover:bg-secondary">
-              <Image className="text-sm duration-1000 group-hover:ease-in-out group-hover:scale-100 group-hover:rounded-t-md" src={team.flag} loading="lazy" alt={team.name} width={32} height={32} />
-              <p className="pt-2 lg:pt
-              -4 lg:px-2 group-hover:pt-2 ease-in-out duration-300 xl:opacity-50 group-hover:xl:opacity-100 w-[200px] text-center text-xs lg:text-md min-h-[60px] max-w-full">{team.name}</p>
-            </div>
-          ))}
-
+        <H1 text="Premier League" />
+        <div className="w-full overflow-x-auto border rounded-lg by-season border-slate-900">
+          <table className="max-w-full overflow-x-auto text-sm text-center hover:cursor-pointer">
+            <thead className="font-light uppercase bg-slate-800">
+              <tr>
+                <th className="sticky top-0 left-0 py-3 pl-1 text-left z-2">Team</th>
+                <th className="px-1">92/93</th>
+                <th className="px-1">93/94</th>
+                <th className="px-1">94/95</th>
+                <th className="px-1">95/96</th>
+                <th className="px-1">96/97</th>
+                <th className="px-1">97/98</th>
+                <th className="px-1">98/99</th>
+                <th className="px-1">99/00</th>
+                <th className="px-1">00/01</th>
+                <th className="px-1">01/02</th>
+                <th className="px-1">02/03</th>
+                <th className="px-1">03/04</th>
+                <th className="px-1">04/05</th>
+                <th className="px-1">05/06</th>
+                <th className="px-1">06/07</th>
+                <th className="px-1">07/08</th>
+                <th className="px-1">08/09</th>
+                <th className="px-1">09/10</th>
+                <th className="px-1">10/11</th>
+                <th className="px-1">11/12</th>
+                <th className="px-1">12/13</th>
+                <th className="px-1">13/14</th>
+                <th className="px-1">14/15</th>
+                <th className="px-1">15/16</th>
+                <th className="px-1">16/17</th>
+                <th className="px-1">17/18</th>
+                <th className="px-1">18/19</th>
+                <th className="px-1">19/20</th>
+                <th className="px-1">20/21</th>
+                <th className="px-1">21/22</th>
+                <th className="px-1">22/23</th>
+                <th className="px-1">23/24</th>
+                <th className="px-1">24/25</th>
+              </tr>
+            </thead>
+            <tbody className="bg-slate-800/50">
+              {EnglishPremierLeagueBySeasons.map((team, index) => (
+                <tr key={index} data-key={index} className="border-b border-slate-800 hover:bg-slate-900 hover:text-yellow-500">
+                  <td className="sticky left-0 text-left z-2 min-w-[200px] pl-1 py-1">{team.teamName}</td>
+                  <td className="border-l border-slate-900" title={team.season1Details}>{team.season1Pos === -1 ? <span className="text-red-500">-</span> : team.season1Pos}</td>
+                  <td className="border-l border-slate-900">{team.season2Pos === -1 ? <span className="text-red-500">-</span> : team.season2Pos}</td>
+                  <td className="border-l border-slate-900">{team.season3Pos === -1 ? <span className="text-red-500">-</span> : team.season3Pos}</td>
+                  <td className="border-l border-slate-900">{team.season4Pos === -1 ? <span className="text-red-500">-</span> : team.season4Pos}</td>
+                  <td className="border-l border-slate-900">{team.season5Pos === -1 ? <span className="text-red-500">-</span> : team.season5Pos}</td>
+                  <td className="border-l border-slate-900">{team.season6Pos === -1 ? <span className="text-red-500">-</span> : team.season6Pos}</td>
+                  <td className="border-l border-slate-900">{team.season7Pos === -1 ? <span className="text-red-500">-</span> : team.season7Pos}</td>
+                  <td className="border-l border-slate-900">{team.season8Pos === -1 ? <span className="text-red-500">-</span> : team.season8Pos}</td>
+                  <td className="border-l border-slate-900">{team.season9Pos === -1 ? <span className="text-red-500">-</span> : team.season9Pos}</td>
+                  <td className="border-l border-slate-900">{team.season10Pos === -1 ? <span className="text-red-500">-</span> : team.season10Pos}</td>
+                  <td className="border-l border-slate-900">{team.season11Pos === -1 ? <span className="text-red-500">-</span> : team.season11Pos}</td>
+                  <td className="border-l border-slate-900">{team.season12Pos === -1 ? <span className="text-red-500">-</span> : team.season12Pos}</td>
+                  <td className="border-l border-slate-900">{team.season13Pos === -1 ? <span className="text-red-500">-</span> : team.season13Pos}</td>
+                  <td className="border-l border-slate-900">{team.season14Pos === -1 ? <span className="text-red-500">-</span> : team.season14Pos}</td>
+                  <td className="border-l border-slate-900">{team.season15Pos === -1 ? <span className="text-red-500">-</span> : team.season15Pos}</td>
+                  <td className="border-l border-slate-900">{team.season16Pos === -1 ? <span className="text-red-500">-</span> : team.season16Pos}</td>
+                  <td className="border-l border-slate-900">{team.season17Pos === -1 ? <span className="text-red-500">-</span> : team.season17Pos}</td>
+                  <td className="border-l border-slate-900">{team.season18Pos === -1 ? <span className="text-red-500">-</span> : team.season18Pos}</td>
+                  <td className="border-l border-slate-900">{team.season19Pos === -1 ? <span className="text-red-500">-</span> : team.season19Pos}</td>
+                  <td className="border-l border-slate-900">{team.season20Pos === -1 ? <span className="text-red-500">-</span> : team.season20Pos}</td>
+                  <td className="border-l border-slate-900">{team.season21Pos === -1 ? <span className="text-red-500">-</span> : team.season21Pos}</td>
+                  <td className="border-l border-slate-900">{team.season22Pos === -1 ? <span className="text-red-500">-</span> : team.season22Pos}</td>
+                  <td className="border-l border-slate-900">{team.season23Pos === -1 ? <span className="text-red-500">-</span> : team.season23Pos}</td>
+                  <td className="border-l border-slate-900">{team.season24Pos === -1 ? <span className="text-red-500">-</span> : team.season24Pos}</td>
+                  <td className="border-l border-slate-900">{team.season25Pos === -1 ? <span className="text-red-500">-</span> : team.season25Pos}</td>
+                  <td className="border-l border-slate-900">{team.season26Pos === -1 ? <span className="text-red-500">-</span> : team.season26Pos}</td>
+                  <td className="border-l border-slate-900">{team.season27Pos === -1 ? <span className="text-red-500">-</span> : team.season27Pos}</td>
+                  <td className="border-l border-slate-900">{team.season28Pos === -1 ? <span className="text-red-500">-</span> : team.season28Pos}</td>
+                  <td className="border-l border-slate-900">{team.season29Pos === -1 ? <span className="text-red-500">-</span> : team.season29Pos}</td>
+                  <td className="border-l border-slate-900">{team.season30Pos === -1 ? <span className="text-red-500">-</span> : team.season30Pos}</td>
+                  <td className="border-l border-slate-900">{team.season31Pos === -1 ? <span className="text-red-500">-</span> : team.season31Pos}</td>
+                  <td className="border-l border-slate-900">{team.season32Pos === -1 ? <span className="text-red-500">-</span> : team.season32Pos}</td>
+                  <td className="border-l border-slate-900">{team.season33Pos === -1 ? <span className="text-red-500">-</span> : team.season33Pos}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-
-        <div className="grid w-full grid-cols-2 gap-8 groups sm:grid-cols-3 lg:grid-cols-6 2xl:gap-16">
-          {groups.map((group, index) => (
-            <div key={index} className="flex flex-col w-full px-4 py-6 text-blue-900 border border-blue-600 rounded-lg group bg-white/90">
-              <h2 className="mb-4 text-sm font-bold tracking-wider text-left uppercase sm:text-center lg:text-left">Group {group}</h2>
-              <div className="flex flex-col justify-center gap-4 group-teams">
-                {Euro2024Teams.filter((team) => team.group === group).map((team, index) => (
-                  <div key={index} className="flex items-center gap-4 team sm:flex-col lg:flex-row">
-                    <Image src={team.flag} alt={team.name} width={32} height={32} />
-                    <p className="text-xs">{team.name}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-
-        </div>
-
-
       </main>
-        <MainFooter />
+      <MainFooter />
     </div>
   );
 }
