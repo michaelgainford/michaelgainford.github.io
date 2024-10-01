02557a0bc93e5995b159173258f465af3da79703
Michael Gainford <m1cha3lg@outlook.com>
2024-10-01 22:59:43 +0100

    Update padding top of global footer

--- components/footer/MainFooter.jsx
+++ components/footer/MainFooter.jsx
@@ -4,7 +4,7 @@ import Copyright from "@/components/footer/Copyright"
 
 export default function MAIN_FOOTER() {
   return (
-    <footer className={`w-[90%] md:w-[92%] lg:-[95%] max-w-[2000px] flex flex-col lg:justify-between gap-1 pt-6 md:mt-8 lg:mt-12 xl:mt-16 2xl:mt-24 mx-auto header-content lg:pb-2 lg:gap-4 tracking-wider`}>
+    <footer className={`w-[90%] md:w-[92%] lg:-[95%] max-w-[2000px] flex flex-col gap-1 pt-6 tracking-wider mx-auto header-content md:mt-8 lg:pb-2 lg:gap-4 lg:justify-between lg:pt-16 2xl:pt-24`}>
       <div className={`grid justify-between w-full grid-cols-1 lg:mb-12 lg:flex-row max-lg:gap-y-8 lg:gap-x-24 lg:grid-cols-4`}>
         <MainFooterNav />
         <MainFooterAbout />
