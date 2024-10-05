e6e08a43adc3b088b5f77ce108837f46b8ee127e
Michael Gainford <m1cha3lg@outlook.com>
2024-10-05 23:41:09 +0100

    Update global header

---
+++ components/header/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import MainLogo from "@/components/header/MainLogo";
+import MainNav from "@/components/header/MainNav";
+import MainMobileNav from "@/components/header/MainMobile";
+import IconHamburgerNav from "@/components/icons/IconHamburgerNav";
+
+export default function HEADER() {
+	const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+
+	const toggleMobileNav = () => {
+		setIsMobileNavVisible(!isMobileNavVisible);
+	};
+
+	return (
+		<header className={`w-full`}>
+			<div
+				className={`header-content w-[90%] md:w-[92%] lg:-[95%] max-w-[2000px] flex flex-row justify-between gap-1 py-6 mx-auto sm:items-center lg:gap-2`}
+			>
+				<MainLogo />
+				<MainNav />
+				<div
+					className={`hamburger-menu-icon cursor-pointer lg:hidden`}
+					onClick={toggleMobileNav}
+				>
+					<IconHamburgerNav />
+				</div>
+				<MainMobileNav
+					isVisible={isMobileNavVisible}
+					toggleVisibility={toggleMobileNav}
+				/>
+			</div>
+		</header>
+	);
+}
