5e6c73da7636d680d856938bc375433e1ca6aec8
Michael Gainford <m1cha3lg@outlook.com>
2024-10-17 23:47:50 +0100

    Updated ReadMe and started work on updating FN page

--- app/projects/gaming/fortnite/page.jsx
+++ app/projects/gaming/fortnite/page.jsx
@@ -12,7 +12,7 @@ export default function PAGE_FORTNITE() {
       	<Header />
       	<main className={`${mainStyles}`}>
 			<H1 text={`Fortnite`} />
-			<div className={`grid w-full grid-cols-1 gap-8 lg:gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols`}>
+			<div className={`grid w-full grid-cols-1 gap-8 lg:gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols hidden`}>
 				{DataForFortnite.map((season, index) => (
 				<div
 					key={index}
@@ -38,6 +38,40 @@ export default function PAGE_FORTNITE() {
 						className={`absolute inset-0 z-0 w-full h-full max-md:hidden`}
 					/>
 					<div className={`absolute inset-0 z-10 w-full h-full duration-300 bg-black bg-opacity-60 group-hover/season:bg-opacity-0`}></div>
+				</div>
+				))}
+			</div>
+
+			<h2 className={`text-sm md:text-base lg:text-lg xl:text-xl mb-4 text-left w-full`}>Seasons</h2>
+			<div className={`snap-x snap-mandatory overflow-x-auto scrollbar-hide w-full flex gap-4 md:gap-8`}>
+				{DataForFortnite.map((season, index) => (
+				<div
+					key={index}
+					className={`flex flex-col justify-start !aspect-[1000/563] relative transition group/season duration-300 min-w-[80dvw] sm:min-w-[70dvw] lg:hover:opacity-100 xl:min-w-[800px] pb-8`}
+				>
+					<div className={`mb-4 image md:mb-6 lg:mb-8`}>
+					<Image
+						src={season.mobileImage}
+						alt={season.name}
+						width={200}
+						height={133}
+						className={` inset-0 z-0 w-full h-full md:hidden object-fit`}
+					/>
+					<Image
+						src={season.desktopImage}
+						alt={season.name}
+						width={400}
+						height={200}
+						className={` inset-0 z-0 w-full h-full max-md:hidden object-fit`}
+					/>
+					<div className={`absolute inset-0 z-10 w-full h-full duration-300 bg-black bg-opacity-60 group-hover/season:bg-opacity-0`}></div>
+					</div>
+
+					<div className={`relative z-20 space-y-2 text-slate-200 invisible group-hover/season:visible group-hover/season:opacity-100 group/content`}>
+						<p className={`uppercase text-base font-medium lg:text-2xl`}>Chapter {season.chapter} Season {season.season}</p>
+						<p className={`text-xs uppercase tracking-widest font-bold lg:text-sm`}>{season.theme}</p>
+						<p className={`text-[10px]`}>({season.startDate} to {season.endDate})</p>
+					</div>
 				</div>
 				))}
 			</div>
