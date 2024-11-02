2b5250afa6d49b636f8807828859d3f3cfe65496
Michael Gainford <m1cha3lg@outlook.com>
2024-11-02 11:37:32 +0000

    Recommit Global Button

--- components/buttons/Button_Global.jsx
+++ components/buttons/Button_Global.jsx
@@ -4,7 +4,7 @@ export default function BUTTON_GLOBAL({ href, label, title, ariaLabel, backgroun
 	return (
 		<Link 
 			aria-label={ariaLabel} 
-			className={`px-8 py-3 mx-auto text-xs tracking-widest transition duration-300 border rounded ${background_colour} ${border_colour} ${hover_background_colour} ${text_colour} ${hover_text_colour} sm:py-4 lg:uppercase xl:text-sm`}
+			className={`px-8 py-3 mx-auto text-xs tracking-widest transition duration-300 border rounded ${background_colour} ${border_colour} ${hover_background_colour} ${text_colour} ${hover_text_colour} sm:py-4 lg:uppercase xl:text-sm 3xl:py-5 3xl:text-base`}
 			href={href} 
 			title={title} 
 		>
