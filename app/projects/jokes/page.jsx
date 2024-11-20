860da94be6d2eab46d8af54d22f55142db736e98
Michael Gainford <m1cha3lg@outlook.com>
2024-11-20 14:55:38 +0000

    Created data file for jokes and added random joke generator.

--- app/projects/jokes/page.jsx
+++ app/projects/jokes/page.jsx
@@ -4,61 +4,7 @@ import Footer from "@/components/footer/Footer";
 import { globalPageStyles } from "@/data/Variables";
 import { mainStyles } from "@/data/Variables";
 import Page_Intro from "@/components/site_elements/Page_Intro";
-
-const jokes = [
-	{
-		setup: "What do you call a fish wearing a crown?",
-		punchline: "A kingfish",
-	},
-	{
-		setup: "Will glass coffins be a success?",
-		punchline: "Remains to be seen",
-	},
-	{
-		setup: "When life gives you melons...",
-		punchline: "you might be dyslexic.",
-	},
-	{
-		setup: "I hate Russian dolls...",
-		punchline: "they're so full of themselves",
-	},
-	{
-		setup: "Geology rocks...",
-		punchline: "but geography is where it's at",
-	},
-	{
-		setup: "My wife told me to stop impersonating a flamingo...",
-		punchline: "I had to put my foot down",
-	},
-	{
-		setup: "So what if I don't know what 'Armageddon' means?",
-		punchline: "It's not the end of the world",
-	},
-	{
-		setup: "The future, the present, and the past walk into a bar...",
-		punchline: "Things got a little tense.",
-	},
-	{
-		setup: "I have a few jokes about unemployed people...",
-		punchline: "but none of them work",
-	},
-	{
-		setup: "You’ll never be as lazy...",
-		punchline: "as whoever named the fireplace",
-	},
-	{
-		setup: "I used to think I was indecisive...",
-		punchline: "but now I'm not too sure",
-	},
-	{
-		setup: "I can’t believe I got fired from the calendar factory...",
-		punchline: "all I did was take a day off",
-	},
-	{
-		setup: "I've just written a song about tortillas...",
-		punchline: "actually, it’s more of a rap",
-	}
-];
+import DataForJokes from "@/data/Data_For_Jokes";
 
 export default function PAGE_JOKES() {
 	return (
@@ -72,7 +18,7 @@ export default function PAGE_JOKES() {
 					other_classes={`text-balance text-center`}
 				/>
 				<div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:gap-16 xl:grid-cols-3 motion motion-preset-slide-up motion-delay-1000">
-					{jokes.map((joke, index) => (
+					{DataForJokes.map((joke, index) => (
 					<div key={index} className="border border-rose-700 bg-rose-900 p-4 rounded-lg min-h-[200px] space-y-4 text-center flex items-center flex-col justify-center">
 						<p className="text-xl font-light uppercase">{joke.setup}</p>
 						<p className="text-lg">{joke.punchline}</p>
