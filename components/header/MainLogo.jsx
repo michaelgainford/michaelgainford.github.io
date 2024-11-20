89276d9e4c99d6acae8675ca733776aa35e1d5b9
Michael Gainford <m1cha3lg@outlook.com>
2024-11-20 14:54:55 +0000

    Update mobile header logo font size

--- components/header/MainLogo.jsx
+++ components/header/MainLogo.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import IconLaptop from "@/components/icons/IconLaptop";
 
-export default function MAIN_LOGO() {
-  return (
-    <Link href="/" className={`flex items-center gap-2 max-md:justify-center`}>
-      <IconLaptop classes={`size-5 fill-amber-500`} />
-      <span className={`text-[13px] font-medium tracking-wider text-left lg:text-lg lg:font-medium xl:text-xl flex`}>
-        <span className={`uppercase`}>MichaelGainford</span>
-        <span>.dev</span>
-      </span>
-    </Link>
-  ) 
+export default function COMPONENT_MAIN_LOGO() {
+	return (
+		<Link href="/" className="flex items-center gap-2 max-md:justify-center">
+			<IconLaptop classes="size-5 fill-amber-500" />
+			<span className="text-[13.245px] font-medium tracking-wider text-left lg:text-lg lg:font-medium xl:text-xl flex">
+				<span className="uppercase">MichaelGainford</span>
+				<span>.dev</span>
+			</span>
+		</Link>
+	) 
 }
