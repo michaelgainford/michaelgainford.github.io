3540cb5bdf04358a6380d542c7537113bfc68c84
Michael Gainford <m1cha3lg@outlook.com>
2024-11-20 14:55:52 +0000

    Add skills icon component.

--- components/icons/Icons_Skills.jsx
+++ components/icons/Icons_Skills.jsx
@@ -4,6 +4,7 @@ import IconTailwindSimple from './IconTailwindSimple'
 import IconJavaScript from './IconJavaScript'
 import IconReact from './IconReact'
 import IconNextJS from './IconNextJS'
+import { headers } from 'next/headers'
 
 const IconsSkillsData = [
     {
@@ -38,14 +39,14 @@ const IconsSkillsData = [
     },
 ]
 
-export default function ICONS_SKILLS({ heading}) {
+export default function ICONS_SKILLS({ heading, heading_styles}) {
     return (
         <div className="col-span-12 px-6 py-12 transition-all duration-500 border rounded-lg border-slate-500/10 md:p-8 2xl:p-16 hover:border-amber-500 hover:bg-slate-500/10">
-            <h2 className="mb-8 text-3xl font-bold lg:mt-8 lg:mb-16">{ heading }</h2>
-            <div className="grid grid-cols-3 xl:grid-cols-6 gap-6 [&>.flex]:justify-center [&>.flex]:items-center [&>.flex]:rounded-lg">
+            <h2 className={heading_styles}>{ heading }</h2>
+            <div className="grid grid-cols-3 md:grid-cols-6 gap-6 [&>.flex]:justify-center [&>.flex]:items-center [&>.flex]:rounded-lg">
                 {IconsSkillsData.map((icon, index) => (
                     <div key={index} className="flex p-4 transition-all duration-500 border border-slate-500/20 aspect-square hover:bg-slate-900 hover:border-amber-500" title={icon.title}>
-                        <icon.icon classes="size-8 md:size-12 lg:size-16" />
+                        <icon.icon classes="size-8 xs:size-12 2xl:size-16" />
                     </div>
                 ))}
             </div>
