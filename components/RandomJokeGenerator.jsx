14894648ed36df5055cf6c2144032b2af952de49
Michael Gainford <m1cha3lg@outlook.com>
2024-11-22 17:31:06 +0000

    Updated styling.

--- components/RandomJokeGenerator.jsx
+++ components/RandomJokeGenerator.jsx
@@ -3,7 +3,7 @@ import DataForJokes from "@/data/Data_For_Jokes";
 export default function RANDOM_JOKES_GENERATOR () {
     const randomJoke = DataForJokes[Math.floor(Math.random() * DataForJokes.length)];
     return (
-        <div className="p-4 rounded-lg xs:min-h-[200px] space-y-4 text-center flex items-center flex-col justify-center">
+        <div className="flex flex-col items-center justify-center p-4 space-y-4 text-center rounded-lg">
             <p className="text-xl font-light uppercase">{randomJoke.setup}</p>
             <p className="text-lg">{randomJoke.punchline}</p>
         </div>
