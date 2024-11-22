ce64867fb00db5b8ba555ba1115fa802f4456551
Michael Gainford <m1cha3lg@outlook.com>
2024-11-22 17:33:46 +0000

    Visual tweaks to icon components.

--- components/icons/Icons_Skills.jsx
+++ components/icons/Icons_Skills.jsx
@@ -41,8 +41,8 @@ const IconsSkillsData = [
 
 export default function ICONS_SKILLS({ heading, heading_styles}) {
     return (
-        <div className="col-span-12 px-6 py-12 transition-all duration-500 border rounded-lg border-slate-500/10 md:p-8 2xl:p-16 hover:border-amber-500 hover:bg-slate-500/10">
-            <h2 className={heading_styles}>{ heading }</h2>
+        <div className="col-span-12 px-6 py-12 transition-all duration-500 border rounded-lg border-slate-500/10 md:p-8 2xl:p-16 hover:border-amber-500 hover:bg-slate-500/10 group/built-using">
+            <h2 className={`${heading_styles} group-hover/built-using:border-amber-500/80`}>{ heading }</h2>
             <div className="grid grid-cols-3 md:grid-cols-6 gap-6 [&>.flex]:justify-center [&>.flex]:items-center [&>.flex]:rounded-lg">
                 {IconsSkillsData.map((icon, index) => (
                     <div key={index} className="flex p-4 transition-all duration-500 border border-slate-500/20 aspect-square hover:bg-slate-900 hover:border-amber-500" title={icon.title}>
--- components/icons/Icons_Socials.jsx
+++ components/icons/Icons_Socials.jsx
@@ -34,13 +34,13 @@ const IconsSocialsData = [
 
 export default function ICONS_SKILLS({ heading, heading_styles}) {
     return (
-        <div data-card="socials" id="socials" className="col-span-12 px-6 py-12 text-xs text-left transition-all duration-500 border rounded-lg lg:px-12 md:col-span-7 something-4 border-slate-500/10 hover:border-amber-500 hover:bg-slate-500/10 group/socials @container">
+        <div data-card="socials" id="socials" className="col-span-12 px-6 py-12 text-xs text-left transition-all duration-500 border rounded-lg lg:px-12 md:col-span-7 something-4 border-slate-500/10 hover:border-amber-500 hover:bg-slate-500/10 group/socials @container/socials">
             <h2 className={`${heading_styles} group-hover/socials:border-amber-500/80`}>{ heading }</h2>
             <div className="grid grid-cols-2 gap-4 lg:gap-8 @3xl:grid-cols-4">
                 {IconsSocialsData.map((icon, index) => (
-                    <Link key={index} href="/" className="flex items-center justify-center border rounded-lg icon aspect-square border-slate-200/5 [&>*]:size-12 [&>*]:fill-current hover:border-amber-500 hover:bg-slate-900 duration-500 transition-all overflow-hidden flex-col relative group/link [&_svg]:size-8">
+                    <Link key={index} href="/" className="flex items-center justify-center border rounded-lg icon aspect-square border-slate-200/5 [&>*]:size-12 [&>*]:fill-current hover:border-amber-500 hover:bg-slate-900 duration-500 transition-all overflow-hidden flex-col relative group/link [&_svg]:size-8 [&_svg]:@lg/socials:size-12">
                         <icon.icon/>
-                        <p className="absolute bottom-0 tracking-wider text-center translate-y-32 group-hover/link:translate-y-0">{icon.title}</p>
+                        <p className="absolute bottom-0 tracking-wider text-center lg:translate-y-32 group-hover/link:lg:translate-y-0">{icon.title}</p>
                     </Link>
                 ))}
             </div>
