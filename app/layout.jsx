46f536a1ee0adf9506c3f95caeb0461dddddac3f
Michael Gainford <m1cha3lg@outlook.com>
2024-11-27 21:40:17 +0000

    Clean up file.

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// This is my default metadata for pages that don't have any metadata
 export const metadata = {
 	title: `Front End Developer, Merseyside, England | MichaelGainford.dev`,
 	description: `Michael's Web Dev Playground`,
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang={`en`} className="scroll-smooth">
+		<html lang="en" className="scroll-smooth">
 			<body className={`${inter.className}`}>{children}</body>
 		</html>
 	);
