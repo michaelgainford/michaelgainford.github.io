57a68b2c59d2ba29b2571bff39b4441ecea8c56d
Michael Gainford <m1cha3lg@outlook.com>
2024-12-17 08:48:49 +0000

    Add accrediation to About Me card component.

--- components/cards/Card_AboutMe.jsx
+++ components/cards/Card_AboutMe.jsx
@@ -10,7 +10,7 @@ export default function COMPONENT_CARD_ABOUT_ME() {
             <p className="text-center lg:text-left max-lg:px-4 text-sm/5 lg:text-lg/8">
                 My name is Michael. I am Front-End Developer with over 15 years experience in a wide range of frontend technologies. I am passionate about creating beautiful and functional websites that are easy to use and accessible to all users. I regularly keep up to date with the latest web technologies and best practices to ensure that I am always delivering the best possible solutions to my clients. I am always looking for new opportunities to work on exciting projects and expand my skillset.
             </p>
-            <div className="flex max-lg:px-4 justify-start mt-8 lg:mt-12 motion motion-preset-slide-up motion-delay-[1200ms]">
+            <div className="flex flex-col md:flex-row max-lg:px-4 justify-start mt-8 lg:mt-12 motion motion-preset-slide-up motion-delay-[1200ms] gap-4">
                 <Button 
                     href="#socials"
                     label="Connect"
@@ -20,7 +20,21 @@ export default function COMPONENT_CARD_ABOUT_ME() {
                     border_colour="border-slate-700" 
                     hover_background_colour="hover:bg-amber-500"
                     text_colour="text-current"
+                    hover_text_colour="hover:text-slate-900 ml-0 mx-auto"
+                    other_classes="max-md:w-full"	
+                />
+                <Button 
+                    href="https://www.credly.com/users/michael-gainford.3546f4c8"
+                    label="Credly Certifications"
+                    target="_blank"
+                    title="Credly Certifications" 
+                    ariaLabel="Credly Certifications"
+                    background_colour="bg-slate-800"
+                    border_colour="border-slate-700" 
+                    hover_background_colour="hover:bg-amber-500"
+                    text_colour="text-current"
                     hover_text_colour="hover:text-slate-900 ml-0 mx-auto"	
+                    other_classes="max-md:w-full"
                 />
             </div>
         </div>
