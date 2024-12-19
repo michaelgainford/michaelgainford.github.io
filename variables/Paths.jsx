90973f4660f8f66017da8d31064577601323c747
Michael Gainford <m1cha3lg@outlook.com>
2024-12-19 18:20:41 +0000

    Created and populated new variables files.

---
+++ variables/Paths.jsx
@@ -0,0 +1,35 @@
+{/********************************
+	INTERNAL URLS
+*********************************/}
+const clubLogosRoot = "/football/premier-league/club-logos";
+const premierLeagueHubUrl = "/projects/football/competitions/english-premier-league";
+const premierLeagueLogoSvgPath = "/football/competition-logos/premier-league-logo-white.svg";
+const premierLeagueHubBlockImageRoot = "/football/premier-league/hub-blocks";
+
+
+{/********************************
+  EXTERNAL URLS
+*********************************/}
+const urlForShopify = "https://www.shopify.com/";
+const urlForNextJS = "https://nextjs.org";
+const urlForReactJS = "https://react.dev";
+const urlForTailwindCSS = "https://tailwindcss.com";
+const urlForMyCredlyPage = "https://www.credly.com/users/michael-gainford.3546f4c8";
+
+
+{/********************************
+	EXPORTS 
+*********************************/}
+export {
+	// internal
+	clubLogosRoot,
+	premierLeagueHubUrl,
+	premierLeagueLogoSvgPath,
+	premierLeagueHubBlockImageRoot,
+	// external
+	urlForShopify, 
+	urlForNextJS, 
+	urlForReactJS, 
+	urlForTailwindCSS,
+	urlForMyCredlyPage
+};
