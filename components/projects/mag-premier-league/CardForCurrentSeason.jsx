c5561563becf28730b427f56056731d1f72fdd10
Michael Gainford <m1cha3lg@outlook.com>
2024-12-20 21:21:37 +0000

    Updated MAGPL cards.

--- components/projects/mag-premier-league/CardForCurrentSeason.jsx
+++ components/projects/mag-premier-league/CardForCurrentSeason.jsx
@@ -1,15 +1,25 @@
 import Button from '@/components/buttons/Button';
-import { magEPLCurrentSeason as currentSeason } from '@/variables/Data';
+import { magPLCurrentSeason as currentSeason } from '@/variables/Data';
 import { magEPL_CardStyles, magEPL_H2Styles } from '@/variables/Styles';
 
 export default function COMPONENT_CARD_FOR_CURRENT_SEASON () {
 	return(
-		<div className={`${magEPL_CardStyles} mg-test border-none col-span-1`}>
-			<h2 className={`${magEPL_H2Styles}`}>Current Season</h2>
-			<div className="flex flex-col items-center justify-center h-full">
+		<div className={magEPL_CardStyles}>
+			<h2 className={`${magEPL_H2Styles} group-hover:bg-magpl group-hover:text-slate-900 transition-all duration-500`}>Current Season</h2>
+			<div className="flex flex-col items-center justify-center h-full p-8">
 				<div className="contents space-y-4">
-					<p className="text-[160px] font-bold leading-none">{currentSeason}</p>
-					<Button label="View Season Info" border="border-magpl" href={`/projects/gaming/the-mag-premier-league/seasons/season-${currentSeason}`} />
+					<p className="text-[160px] font-bold leading-none text-white/70 group-hover:text-white">{currentSeason}</p>
+					<Button 
+						ariaLabel={`View Season ${currentSeason} Info`}
+						background_colour="bg-transparent"
+						border_colour="border-magpl/20 group-hover:border-magpl-500"
+						hover_background_colour="hover:bg-magpl-500"
+						hover_text_colour="hover:text-slate-900"
+						label={`View Season ${currentSeason}`}
+						href={`/projects/gaming/the-mag-premier-league/seasons/season-${currentSeason}`} 
+						text_colour="text-white"
+						title={`View Season ${currentSeason} Info`}
+					/>
 				</div>
 			</div>
 		</div>		
--- components/projects/mag-premier-league/CardForUpcomingFixtures.jsx
+++ components/projects/mag-premier-league/CardForUpcomingFixtures.jsx
@@ -1,9 +1,20 @@
 import { magEPL_CardStyles, magEPL_H2Styles } from '@/variables/Styles';
+import { magEPLUpcomingMatches } from '@/variables/Data';
 
 export default function COMPONENT_CARD_FOR_PAST_WINNERS () {
 	return(
 		<div className={magEPL_CardStyles}>
-			<h2 className={`${magEPL_H2Styles}`}>Upcoming Fixtures</h2>
+			<h2 className={magEPL_H2Styles}>
+				Upcoming Fixtures
+			</h2>
+			<ul className="divide-y divide-slate-800">
+				{magEPLUpcomingMatches.map((fixture, index) => (
+				<li key={index} className="flex items-center justify-center py-4 flex-col h-1/3">
+					<span className="text-lg uppercase">{fixture.homeTeam} v {fixture.awayTeam}</span>
+					<span className="text-sm">{fixture.date}</span>
+				</li>
+				))}
+			</ul>
 		</div>	
 	)
 }
