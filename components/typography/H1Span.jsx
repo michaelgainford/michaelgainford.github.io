31a2af58bde0ee02e268e3f38006abb6173ede74
Michael Gainford <m1cha3lg@outlook.com>
2024-12-20 21:22:20 +0000

    Updated H1 component.

--- components/typography/H1Span.jsx
+++ components/typography/H1Span.jsx
@@ -1,6 +1,6 @@
-export default function H1_SPAN({ text, font_sizes, other_classes }) {
+export default function H1_SPAN({ font_sizes, other_classes, text }) {
   return (
-    <h1 className={`w-full font-light leading-relaxed tracking-wide text-center text-md motion motion-preset-slide-up motion-delay-300 sm:text-md md:text-xl max-lg:mb-4 max-lg:pt-8 max-lg:font-bold lg:pb-16 lg:font-medium lg:text-3xl ${font_sizes} ${other_classes}`}>
+    <h1 className={`w-full font-light leading-relaxed tracking-wide text-center text-md md:text-xl max-lg:mb-4 max-lg:pt-8 max-lg:font-bold lg:pb-16 lg:font-medium lg:text-3xl ${font_sizes} ${other_classes}`}>
       {text}
     </h1>
   )
