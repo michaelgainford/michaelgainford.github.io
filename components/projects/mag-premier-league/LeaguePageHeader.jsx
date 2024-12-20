5f1b2ff4d6cd0b44d9e1c36ade348e438c57535f
Michael Gainford <m1cha3lg@outlook.com>
2024-12-20 21:22:02 +0000

    Turn header into component.

--- components/projects/mag-premier-league/LeaguePageHeader.jsx
+++ components/projects/mag-premier-league/LeaguePageHeader.jsx
@@ -3,10 +3,16 @@ import H1 from "@/components/typography/H1Span";
 
 export default function COMPONENT_LEAGUE_PAGE_HEADER() {
 	return (
-		<div className="max-lg:mt-4">
-			<div className={`flex flex-col items-center gap-2 lg:gap-4`}>
-				<IconMagPremierLeague classes={`mx-auto size-24 md:size-32 lg:size-48`} />
-				<H1 text={`The MAG Premier League`} font_sizes={``} other_classes={`text-balance! font-bold! text-balance uppercase font-bold! text-slate-300 max-w-[20ch] mb-0! pb-0! pt-0!`} />
+		<div className="max-lg:mt-4 border-b border-slate-900 w-full pb-8 lg:pb-12">
+			<div className="flex flex-col items-center gap-2">
+				<IconMagPremierLeague 
+					classes="mx-auto size-24 md:size-32 lg:size-48"
+				/>
+				<H1 
+					font_sizes="" 
+					text="The MAG Premier League" 
+					other_classes="text-balance! font-bold! text-balance uppercase font-bold! text-slate-300  mb-0! pb-0! pt-0! leading-5 lg:leading-[1.1em]!"
+				/>
 			</div>
 		</div>
 	)
