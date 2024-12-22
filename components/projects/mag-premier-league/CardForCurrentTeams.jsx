3b295746dcc987757d767e7027aa1d1f2847aad5
Michael Gainford <m1cha3lg@outlook.com>
2024-12-22 09:21:58 +0000

    Fixed text sizing in Current Teams card.

--- components/projects/mag-premier-league/CardForCurrentTeams.jsx
+++ components/projects/mag-premier-league/CardForCurrentTeams.jsx
@@ -6,13 +6,13 @@ export default function COMPONENT_CARD_FOR_CURRENT_TEAMS () {
 			<h2 className={`${magEPL_H2Styles}`}>Current Teams</h2>
 			<div className="w-full flex flex-row lg:flex-col gap-8 justify-center p-8">
 				<div className="text-white bg-red-800 team mx-auto lg:opacity-50 lg:hover:opacity-100 lg:transition-all lg:duration-300 w-full rounded-md">
-					<span className={`text-2xl lg:text-lg py-4 flex justify-center`}>Alfie</span>
+					<span className={`text-sm lg:text-lg py-4 flex justify-center`}>Alfie</span>
 				</div>
 				<div className="text-white bg-blue-800 team mx-auto lg:opacity-50 lg:hover:opacity-100 lg:transition-all lg:duration-300 w-full rounded-md">
-					<span className={`text-2xl lg:text-lg py-4 flex justify-center`}>Garcon</span>
+					<span className={`text-sm lg:text-lg py-4 flex justify-center`}>Garcon</span>
 				</div>
 				<div className="text-white bg-black team mx-auto lg:opacity-50 lg:hover:opacity-100 lg:transition-all lg:duration-300 w-full rounded-md">
-					<span className={`text-2xl lg:text-lg py-4 flex justify-center`}>Michael</span>
+					<span className={`text-sm lg:text-lg py-4 flex justify-center`}>Michael</span>
 				</div>
 			</div>
 		</div>
