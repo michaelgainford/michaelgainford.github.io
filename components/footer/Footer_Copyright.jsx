ab0f560c56c7057150ed411d1ec337148d8f7635
Michael Gainford <m1cha3lg@outlook.com>
2024-12-31 18:38:31 +0000

    Update year.

--- components/footer/Footer_Copyright.jsx
+++ components/footer/Footer_Copyright.jsx
@@ -1,7 +1,7 @@
 export default function FOOTER_COPYRIGHT() {
 	return (
     	<div className={`pt-4 text-[10px] pb-1 text-center copyright border-slate-700`}>
-      		<p>Built by MG 2024</p>
+      		<p>Built by MG &mdash; &copy; 2025</p>
     	</div>
   	)
 }
