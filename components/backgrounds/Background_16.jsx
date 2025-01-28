bb65bec6ed13afc405375dc10824cdbf9ab6b8b7
Michael Gainford <m1cha3lg@outlook.com>
2025-01-28 07:11:05 +0000

    Reformatted backgrounds components.

--- components/backgrounds/Background_16.jsx
+++ components/backgrounds/Background_16.jsx
@@ -1,8 +1,7 @@
-export default function COMPONENT_BACKGROUND_9 () {
+export default function Component_Background16() {
 	return (
 		<div className="relative h-full w-full bg-neutral-900">
-			<div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]">
-			</div>
+			<div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
 		</div>
-	)
+	);
 }
