594480fcb3cda8c742d8a1169230dac255685b0c
Michael Gainford <m1cha3lg@outlook.com>
2025-02-23 17:36:29 +0000

    Sitemap/maintenance tracker update.

--- components/pigeon/TableOfSitePages.jsx
+++ components/pigeon/TableOfSitePages.jsx
@@ -11,7 +11,7 @@ export default function Component_TableOfSitePages() {
 	return (
 		<div className="overflow-x-auto">
 			<table className="w-full max-w-[1800px] mx-auto mt-8 mb-16 text-left text-md xl:max-w-[90dvw]">
-				<thead className="text-xs lg:text-sm">
+				<thead className="text-xxs lg:text-sm">
 					<tr className="text-left border-b-4 border-slate-200/5 text-amber-500/70 [&>th]:py-2 [&>th]:text-center">
 						<th className="py-2 text-left! pl-2">Page</th>
 						<th className="py-2 text-left!">Link Path</th>
@@ -21,7 +21,7 @@ export default function Component_TableOfSitePages() {
 						<th className="text-left!">Status</th>
 					</tr>
 				</thead>
-				<tbody className="text-xs xl:text-sm">
+				<tbody className="text-xxs xl:text-sm">
 					{PagesInSitemap.map((item, index) => (
 						<tr
 							key={index}
@@ -62,7 +62,7 @@ export default function Component_TableOfSitePages() {
 								)}
 							</td>
 							<td>
-								<span className="px-2 py-1 font-bold w-full tracking-wider text-center rounded-lg relative">
+								<span className="px-2 py-1 font-bold w-full tracking-wider text-center rounded-lg relative flex">
 									<span
 										className={`absolute left-0 top-1/2 transform -translate-y-1/2 w-2 h-2 rounded-full ${
 											Math.floor(
