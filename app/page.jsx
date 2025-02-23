ad7501f113d1d4a5b0e63b0afce3db5432859fe1
Michael Gainford <m1cha3lg@outlook.com>
2025-02-23 17:35:25 +0000

    Home page component updates.

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,8 @@ import {
 	globalWrapperFixedWidth 
 } from "@/variables/Styles";
 import Header from "@/components/header/_Header";
-import HomeResources from "@/components/page_home/Home_Resources";
-import HomeProjects from "@/components/page_home/Home_Projects";
+import HomeResources from "@/components/page_specific/home/Home_Resources";
+import HomeProjects from "@/components/page_specific/home/Home_Projects";
 import Footer from "@/components/footer/_Footer";
 
 export default function Page_Home() {
