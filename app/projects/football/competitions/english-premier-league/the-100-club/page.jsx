e3b6df8dfe5e05403b72c36402f5862739a68ef7
Michael Gainford <m1cha3lg@outlook.com>
2025-12-05 22:34:00 +0000

    Updated 100 club as of 05/12/2025.

--- app/projects/football/competitions/english-premier-league/the-100-club/page.jsx
+++ app/projects/football/competitions/english-premier-league/the-100-club/page.jsx
@@ -53,7 +53,7 @@ export default function Page_EnglishPremierLeague100Club() {
 				</div>
 				<div className={`${globalWrapperFixedWidth} mt-8`}>
 					<p className="text-center text-[10px] text-epl-400/60 uppercase italic">
-						* Correct as of 22nd February 2025 @ 21:00 (GMT)
+						* Correct as of 5th December 2025 @ 22:30 (GMT)
 					</p>
 				</div>
 			</main>
