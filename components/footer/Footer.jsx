714ef7cb69e286e68fc4ea5360e37d83338c6352
michaelgainford <m1cha3lg@outlook.com>
2026-02-26 15:25:10 +0000

    [update/2026-03-01-v2]: Push updates with 404 page updated and dynamic pages added for Premier League teams (WIP).

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Component_Footer(customTextColour) {
 		<footer className="w-full">
 			<div className="w-[90%] max-w-[2000px] flex flex-col gap-1 pt-6 tracking-wider mx-auto max-md:mt-16 md:w-[92%] md:mt-8 lg:pb-2 lg:gap-4 lg:justify-between lg:pt-16 2xl:pt-24">
 				<div 
-				  className="grid justify-between w-full grid-cols-1 gap-y-8 sm:gap-y-12 lg:mb-12 lg:flex-row lg:gap-x-24 lg:grid-cols-4 border border-rose-500 ">
+				  className="grid justify-between w-full grid-cols-1 gap-y-8 sm:gap-y-12 lg:mb-12 lg:flex-row lg:gap-x-24 lg:grid-cols-4">
 					<FooterNav className={customTextColour}/>
 					<FooterAbout className={customTextColour} />
 				</div>
