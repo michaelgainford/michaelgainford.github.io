2bb45a6cc97d7cb2ac244342ca6c31e88c564836
michaelgainford <m1cha3lg@outlook.com>
2026-06-06 20:32:28 +0100

    [update/2026-06-07]: Updated Next config to fix local dev terminal message.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const rootDir = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
     output: "export",
     basePath: "",
+    allowedDevOrigins: ["192.168.1.71"],
     turbopack: {
         root: rootDir
     },
